19aa5832b60fbe2d79960cb02a4d4867622cbbf0
Gerhard Molin <gerhard@minimumbadass.com>
2022-01-07 10:57:59 +0100

    basic character struct and construction done

--- contracts/EpicGame.sol
+++ contracts/EpicGame.sol
@@ -8,9 +8,53 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
 contract EpicGame {
+	/*
+		Here we create a struct, that holds the attributed of each character.
+	*/
+	struct CharacterAttributes {
+		uint characterIndex;
+		string name;
+		string imageURI;
+		uint hp;
+		uint maxHp;
+		uint darkMatter;
+		uint maxdarkMatter;
+		uint attackDamage;
+	}
 
-    constructor() {
-        console.log("Deploying a EpicGame");
+	/*
+		An array that holds the default data of our characters.
+		This will be helpful when we mint new characters, and need to know
+		the specific attributes like darkMatter, hp, ...
+	*/
+	CharacterAttributes[] defaultCharakters;
+
+	/*
+  		Data passed in to the contract when it's first created initializing the characters.
+  		We're going to actually pass these values in from run.js.
+	*/
+    constructor(
+		string[] memory characterNames,
+		string[] memory characterImageURIs,
+		uint[] memory characterHp,
+		uint[] memory characterDarkMatter,
+		uint[] memory characterAttackingDmg
+	) {
+        for(uint i = 0; i < characterNames.length; i++) {
+			defaultCharakters.push(CharacterAttributes({
+				characterIndex: i,
+				name: characterNames[i],
+				imageURI: characterImageURIs[i],
+				hp: characterHp[i],
+				maxHp: characterHp[i],
+				darkMatter: characterDarkMatter[i],
+				maxdarkMatter: characterDarkMatter[i],
+				attackDamage: characterAttackingDmg[i]
+			}));
+			
+			CharacterAttributes memory c = defaultCharakters[i];
+			console.log("Done initializing %s w/ HP %s, img %s", c.name, c.hp, c.imageURI);
+		}
     }
 
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,14 @@
 const testContract = async () => {
 	const gameContractFactory = await hre.ethers.getContractFactory('EpicGame');
-	const gameContract = await gameContractFactory.deploy();
+	const gameContract = await gameContractFactory.deploy(
+		["Lucifer", "Solulu", "Melanios"],       // Names
+		["https://i.imgur.com/pKd5Sdk.png", // Images
+		"https://i.imgur.com/xVu4vFL.png", 
+		"https://i.imgur.com/WMB6g9u.png"],
+		[600, 400, 250],                    // HP values
+		[130, 260, 400],					// Dark matter values
+		[150, 200, 300]                       // Attack damage values
+	);
 	await gameContract.deployed();
 	console.log('Contract deployed to:', gameContract.address);
 }
