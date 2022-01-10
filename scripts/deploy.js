4ea82a63606e0f4dd0ccec45a2b00d4c9a3f1fdf
Gerhard Molin <gerhard@minimumbadass.com>
2022-01-10 11:38:05 +0100

    deployed web ready smart contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -16,28 +16,26 @@ const deployContract = async () => {
 	await gameContract.deployed();
 	console.log('Contract deployed to:', gameContract.address);
 
-	let txn;
-	txn = await gameContract.mintCharacterNFT(0);
-	await txn.wait();
-	console.log("Minted NFT #1");
+	// let txn;
+	// txn = await gameContract.mintCharacterNFT(0);
+	// await txn.wait();
+	// console.log("Minted NFT #1");
   
-	txn = await gameContract.mintCharacterNFT(1);
-	await txn.wait();
-	console.log("Minted NFT #2");
+	// txn = await gameContract.mintCharacterNFT(1);
+	// await txn.wait();
+	// console.log("Minted NFT #2");
   
-	txn = await gameContract.mintCharacterNFT(2);
-	await txn.wait();
-	console.log("Minted NFT #3");
+	// txn = await gameContract.mintCharacterNFT(2);
+	// await txn.wait();
+	// console.log("Minted NFT #3");
   
-	txn = await gameContract.mintCharacterNFT(1);
-	await txn.wait();
-	console.log("Minted NFT #4");
+	// txn = await gameContract.mintCharacterNFT(1);
+	// await txn.wait();
+	// console.log("Minted NFT #4");
   
-	console.log("Done deploying and minting!");
-
-	txn = await gameContract.attackBoss();
-	await txn.wait();
-	txn = await gameContract.attackBoss();
+	// txn = await gameContract.attackBoss();
+	// await txn.wait();
+	// txn = await gameContract.attackBoss();
 
 }
 
