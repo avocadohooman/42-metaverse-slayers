fcc238e58574ed492122f319654decff6971010c
Gerhard Molin <gerhard@minimumbadass.com>
2022-01-10 11:51:34 +0100

    connect wallet button and business logic done

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,102 @@
-import React from 'react';
-import twitterLogo from './assets/twitter-logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
+import { ethers } from "ethers";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
-          <p className="sub-text">Team up to protect the Metaverse!</p>
-          <div className="connect-wallet-container">
-            <img
-              src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
-              alt="Monty Python Gif"
-            />
-          </div>
-        </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built with @${TWITTER_HANDLE}`}</a>
-        </div>
-      </div>
-    </div>
-  );
+
+	const [currentAccount, setCurrentAccount] = useState('');
+
+	// Here we check if a wallet is connected
+	const checkIfWalletIsConnected = async () => {
+		try {
+			const { ethereum } = window;
+			if (!ethereum) {
+				window.alert("Make sure you have MetaMask!");
+				return ;
+			}
+			console.log("We have the ehtereum object", ethereum);
+			const address = await ethereum.enable(); 
+			console.log('address', address);
+			const accounts = await ethereum.request({method: 'eth_accounts'});
+			const chainId = await ethereum.request({ method: 'eth_chainId' });
+			console.log("Connected to chain " + chainId);
+
+			// String, hex code of the chainId of the Rinkebey test network
+			const rinkebyChainId = "0x4"; 
+			if (chainId !== rinkebyChainId) {
+				alert("You are not connected to the Rinkeby Test Network!");
+			}
+			
+			if (accounts.length > 0) {
+				const account = accounts[0];
+				console.log('found an authorized account: ', account);
+				setCurrentAccount(account);
+			} else {
+				console.log('no authorized account found');
+			}
+		} catch (error) {
+			console.error(error.message);
+		}
+	};
+
+	/*
+	* Implement your connectWallet method here
+	*/
+	const connectWallet = async () => {
+		try {
+			const { ethereum } = window;
+			if (!ethereum) {
+				window.alert("Make sure you have MetaMask!");
+				return ;
+			}
+			const address = await ethereum.enable(); 
+			console.log('address', address);
+			const accounts = await ethereum.request({method: 'eth_accounts'});
+			const chainId = await ethereum.request({ method: 'eth_chainId' });
+			console.log("Connected to chain " + chainId);
+
+			// String, hex code of the chainId of the Rinkebey test network
+			const rinkebyChainId = "0x4"; 
+			if (chainId !== rinkebyChainId) {
+				alert("You are not connected to the Rinkeby Test Network!");
+			}
+
+			if (accounts.length > 0) {
+				const account = accounts[0];
+				console.log('found an authorized account: ', account);
+				setCurrentAccount(account);
+			} else {
+				console.log('no authorized account found');
+			} 
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	return (
+		<div className="App">
+		<div className="container">
+			<div className="header-container">
+			<p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
+			<p className="sub-text">Team up to protect the Metaverse!</p>
+			<div className="connect-wallet-container">
+				<img
+				src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
+				alt="Monty Python Gif"
+				/>
+			</div>
+			<button
+              className="cta-button connect-wallet-button"
+              onClick={connectWallet}
+            >
+              Connect Wallet To Get Started
+            </button>
+			</div>
+		</div>
+		</div>
+	);
 };
 
 export default App;
