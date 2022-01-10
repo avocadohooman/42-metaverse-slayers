30be0d5fc1c3b180089ba03c27fe322217aff35f
Gerhard Molin <gerhard@minimumbadass.com>
2022-01-10 18:25:58 +0100

    adding selectcharacter component and adding renderContent logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,48 @@
 import React, {useEffect, useState} from "react";
 import './App.css';
 import { ethers } from "ethers";
+import SelectCharacter from "./Components/SelectCharacter/SelectCharacter";
 
 // Constants
 
 const App = () => {
 
 	const [currentAccount, setCurrentAccount] = useState('');
+	/*
+	* Right under current account, setup this new state property
+	*/
+	const [characterNFT, setCharacterNFT] = useState(null);
+
+
+	// Render Methods
+	const renderContent = () => {
+		/*
+			Render scenario #1: there is no current account
+		*/
+		if (!currentAccount) {
+			return (
+				<div className="connect-wallet-container">
+				<img
+					src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
+					alt="Monty Python Gif"
+				/>
+				<button
+					className="cta-button connect-wallet-button"
+					onClick={checkIfWalletIsConnected}
+				>
+					Connect Wallet To Get Started
+				</button>
+				</div>
+			);
+		/*
+			Render scenario #2: there is an account, but no NFT character
+		*/
+	} else if (currentAccount && !characterNFT) {
+		return (
+			 <SelectCharacter setCharacterNFT={setCharacterNFT} />
+		);
+	}
+	};
 
 	// Here we check if a wallet is connected
 	const checkIfWalletIsConnected = async () => {
@@ -79,20 +115,9 @@ const App = () => {
 		<div className="App">
 		<div className="container">
 			<div className="header-container">
-			<p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
-			<p className="sub-text">Team up to protect the Metaverse!</p>
-			<div className="connect-wallet-container">
-				<img
-				src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
-				alt="Monty Python Gif"
-				/>
-			</div>
-			<button
-              className="cta-button connect-wallet-button"
-              onClick={connectWallet}
-            >
-              Connect Wallet To Get Started
-            </button>
+				<p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
+				<p className="sub-text">Team up to protect the Metaverse!</p>
+				{renderContent()}
 			</div>
 		</div>
 		</div>
